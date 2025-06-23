8a8b1f0e00ea21631581f1e64f31b2b42fa487de
Denis PC <zedik.xaxaxa@gmail.com>
2025-06-23 17:22:56 +0700

    auth fix v4

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -37,11 +37,6 @@ export const auth = betterAuth({
             clientSecret: getEnvVar('GOOGLE_CLIENT_SECRET') as string,
         }
     },
-    advanced: {
-        crossSubDomainCookies: {
-            enabled: true,
-        },
-    },
     trustedOrigins: [
         "http://localhost:3000",
         "https://solid-test-mu.vercel.app",
